cc82c4ffc2f507adecaf48520c87c3105f9f28fb
Darya <daryakuzmich0@gmail.com>
2022-12-05 21:22:46 +0300

    Выполнила задание № 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая на вход принимает два числа и выдает какое число большее, а какое меньшее.
+Console.WriteLine("Введите число A:");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+if (NumberA > NumberB)
+{
+    Console.WriteLine( NumberA+ " > " + NumberB);
+}
+else if (NumberB > NumberA)
+{
+    Console.WriteLine( NumberA+ " < " + NumberB);
+}
+else
+{
+    Console.WriteLine( NumberA+ " = " + NumberB);
+}
